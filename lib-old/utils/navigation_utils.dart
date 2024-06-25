87851991b41bbfdad593b7916dcc640481e6022b
spadr <enbunn1626@yahoo.co.jp>
2024-06-25 13:40:50 +0900

    fix #17 v2

--- lib-old/utils/navigation_utils.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-void navigateTo(BuildContext context, Widget target) {
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => target),
-  );
-}
