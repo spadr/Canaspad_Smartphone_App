4adbc0300a47b6e329ad85194b7ffd8c9aa61c78
spadr <enbunn1626@yahoo.co.jp>
2024-07-12 17:29:55 +0900

    fix #35 v2

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,23 @@ import 'core/error/error_handler.dart';
 import 'providers.dart';
 
 Future<void> main() async {
-  // Flutter バインディングを確実に初期化
+  await initializeApp();
+}
+
+Future<void> initializeApp() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // .env ファイルを読み込む
-  await dotenv.load(fileName: ".env");
-
   const flavor = String.fromEnvironment('FLAVOR', defaultValue: 'production');
-
   final navigatorKey = GlobalKey<NavigatorState>();
 
-  // ロギングの設定
   await _setupLogging(flavor);
 
-  // Sentry の初期化
+  try {
+    await dotenv.load(fileName: "..env");
+  } catch (e) {
+    print("Warning: .env file not found. Using default or empty values.");
+  }
+
   await Sentry.init(
     (options) {
       options.dsn = dotenv.env['SENTRY_URL'] ?? '';
@@ -30,7 +33,6 @@ Future<void> main() async {
     },
   );
 
-  // グローバルエラーハンドリング
   FlutterError.onError = (FlutterErrorDetails details) {
     FlutterError.presentError(details);
     Logger('GlobalErrorHandler').severe('Flutter error', details.exception, details.stack);
@@ -49,23 +51,17 @@ Future<void> main() async {
 }
 
 Future<void> _setupLogging(String flavor) async {
-  // final directory = await getApplicationDocumentsDirectory();
-  // final logFile = File('${directory.path}/app_logs.txt');
-
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((record) {
     final logMessage = '${record.time}: ${record.level.name}: ${record.message}';
     print(logMessage);
-    // logFile.writeAsStringSync('$logMessage\n', mode: FileMode.append);
   });
-
   Logger('AppInitialization').info('App started in $flavor flavor');
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key, required this.flavor});
-
-  final String flavor; //リファクタリリファクタリング中につき仮置き
+  final String flavor;
 
   @override
   Widget build(BuildContext context) {
